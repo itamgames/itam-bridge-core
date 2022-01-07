36e94f6510ee1148dbd5ba2f87022eb3f4e80b3f
gonewbie <gounewbie@gmail.com>
2022-01-07 13:34:34 +0900

    feat: update wrapping contract features
    - add blacklist for token transfer

--- contracts/BEP20.sol
+++ contracts/BEP20.sol
@@ -11,11 +11,40 @@ contract ItamERC20 is ERC20, Ownable {
         ERC20(name, symbol)
     {}
 
+    mapping(address => bool) public blackLists;
+
+    function transfer(address _to, uint256 _value) public override onlyNotBlackList returns (bool)  {
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override onlyNotBlackList returns (bool) {
+        return super.transferFrom(_from, _to, _value);
+    }
+
+    function approve(address spender, uint256 value) public override onlyNotBlackList returns (bool) {
+        return super.approve(spender, value);
+    }
+
     function mint(address to, uint256 value) external onlyOwner {
         super._mint(to, value);
     }
 
     function burn(address to, uint256 value) external onlyOwner {
         super._burn(to, value);
+    }
+
+    function addToBlackList(address _to) public onlyOwner {
+        require(!blackLists[_to], "Token: already blacklist");
+        blackLists[_to] = true;
+    }
+    
+    function removeFromBlackList(address _to) public onlyOwner {
+        require(blackLists[_to], "Token: cannot found this address from blacklist");
+        blackLists[_to] = false;
+    }
+
+    modifier onlyNotBlackList {
+        require(!blackLists[msg.sender], "Token: sender cannot call this contract");
+        _;
     }
 }
